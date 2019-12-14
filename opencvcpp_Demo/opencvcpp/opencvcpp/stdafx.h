bf54579c205700c78cfcd6c2f9f203bf9e1d5cbf
hezongfanghuaok <754376164@qq.com>
2019-12-14 10:05:55 +0800

    test .igitn

---
+++ opencvcpp_Demo/opencvcpp/opencvcpp/stdafx.h
@@ -0,0 +1,28 @@
+#pragma once
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+
+#include "opencv2/highgui/highgui.hpp"  
+#include <opencv2/core/core.hpp>
+#include <opencv2/gpu/gpu.hpp>	
+#include "opencv2/imgproc/imgproc.hpp"  
+
+#pragma comment(lib,"opencv_highgui2413d.lib")
+#pragma comment(lib,"opencv_core2413d.lib")
+#pragma comment(lib,"opencv_imgproc2413d.lib") 
+
+#include <fstream>
+#include <io.h>
+
+
+
+// TODO:  在此处引用程序需要的其他头文件
